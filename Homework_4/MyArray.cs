4fa6438ca375ae18dda496aeca40f6e987b02b05
Valeriya Bushueva <valeriya_sityaeva1995@mail.ru>
2021-03-05 17:10:24 +0300

    homework4

---
+++ Homework_4/MyArray.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.IO;
+
+namespace Homework_4
+{
+    //  Реализовать конструктор, создающий массив заданной размерности и заполняющий массив числами от начального значения с заданным шагом. 
+    // Создать свойство Sum, которые возвращают сумму элементов массива, метод Inverse, меняющий знаки у всех элементов массива, метод Multi,
+    //  умножающий каждый элемент массива на определенное число, свойство MaxCount, возвращающее количество максимальных элементов.
+    class MyArray
+    {
+        private int[] a;
+
+        // n - размер массива. element - число, которым будет заполнен массив
+        public MyArray(int n, int element)
+        {
+            a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = element;
+            }
+        }
+
+        // Конструктор, создающий массив заданной размерности и заполняющий массив числами от начального значения с заданным шагом
+        public MyArray(int n, int firstElement, int step)
+        {
+            a = new int[n];
+            a[0] = firstElement;
+            for (int i = 0; i < n; i++)
+            {
+                if (i != 0)
+                    a[i] = a[i - 1] + step;
+            }
+        }
+
+        //Конструктор, считывающий массив из файла
+        public MyArray(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                string[] ss = File.ReadAllLines(fileName);
+                a = new int[ss.Length];
+                for (int i = 0; i < ss.Length; i++)
+                {
+                    a[i] = int.Parse(ss[i]);
+                }
+            }
+            else Console.WriteLine("Ошибка! Файл не найден!");
+        }
+
+        // Свойство возвращает максимальный элемент массива
+        public int Max
+        {
+            get
+            {
+                int max = a[0];
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i] > max) max = a[i];
+                }
+
+                return max;
+            }
+        }
+
+        //Свойство возвращает сумму элементов массива
+        public int Sum
+        {
+            get
+            {
+                int sum = 0;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    sum += a[i];
+                }
+
+                return sum;
+            }
+        }
+
+        // Метод меняет знаки у всех элементов массива
+        public int Inverse
+        {
+            set
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    a[i] = a[i] * -1;
+                }
+            }
+        }
+
+        // Метод умножает каждый элемент массива на определенное число
+        public int Multi
+        {
+            set
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    a[i] = a[i] * value;
+                }
+            }
+        }
+
+        //Свойство возвращает минимальный элемент массива
+        public int Min
+        {
+            get
+            {
+                int min = a[0];
+                for (int i = 1; i < a.Length; i++)
+                {
+                    if (a[i] < min) min = a[i];
+                }
+
+                return min;
+            }
+        }
+
+        //Свойство возвращает число положительных элементов массива
+        public int CountPositiv
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i] > 0) count++;
+                }
+
+                return count;
+            }
+        }
+
+
+        //Свойство возвращает количество максимальных элементов
+        public int MaxCount
+        {
+            get
+            {
+                int max = Max;
+                int count = 0;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i] == max) count++;
+                }
+
+                return count;
+            }
+        }
+
+        //Метод, возвращающий строковое представление массива
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach (int v in a)
+            {
+                s = s + v + " ";
+            }
+
+            return s;
+        }
+
+        //Метод, записывающий массив в файл
+        public static void SaveIntoFile(int[] a, string filename)
+        {
+            StreamWriter wr = new StreamWriter(filename);
+            wr.Write(a.Length);
+            for (int i = 0; i < a.Length; i++)
+            {
+                wr.WriteLine(a[i]);
+            }
+
+            wr.Close();
+        }
+
+        // Метод, загружающий массив из файла
+        public static int[] LoadFromFile(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                StreamReader reader = new StreamReader(filename);
+                string str = reader.ReadLine();
+                int.TryParse(str, out int len);
+                int[] a = new int [len];
+                for (int i = 0; i < len; i++)
+                {
+                    int.TryParse(reader.ReadLine(), out int num);
+                    a[i] = num;
+                }
+
+                reader.Close();
+                return a;
+            }
+
+            throw new Exception("Файл не найден!!!");
+        }
+    }
+}
---
+++ Homework_4/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+
+
+// Бушуева Валерия
+namespace Homework_4
+{
+    internal class Program
+    {
+        #region Task1
+        //Дан целочисленный массив из 20 элементов. Элементы массива могут принимать целые значения от –10 000 до 10 000 включительно.
+        //Написать программу, позволяющую найти и вывести количество пар элементов массива, в которых хотя бы одно число делится на 3.
+        //В данной задаче под парой подразумевается два подряд идущих элемента массива. Например, для массива из пяти элементов: 6; 2; 9; –3; 6 – ответ: 4.
+        
+        //Метод подсчёта количества пар элементов массива, в которых хотя бы одно число делится на 3
+        static int GetNumberOfPairs(int[] array, int length)
+        {
+            int amountOfPairs = 0;
+            for (int i = 0; i < length-1; i++)
+            {
+                if (array[i] % 3 == 0 || array[i+1] % 3 == 0)
+                {
+                    amountOfPairs++;
+                }
+            }
+            return amountOfPairs;
+        }
+        #endregion
+        
+        public static void Main(string[] args)
+        {
+           TASK1();
+           TASK2();
+           TASK4();
+           
+        }
+
+        public static void TASK1()
+        {
+            Console.Title = " Программа 1. Найти и вывести количество пар элементов массива, в которых хотя бы одно число делится на 3.";
+            
+            const int arrayLenght = 20;
+            int[] myArray = new int[arrayLenght];
+            Random random = new Random();
+            int result;
+            
+            Console.WriteLine("Программа подсчёта пар элементов, в которых хотя бы одно число делится на 3.");
+            Console.Write("\nВ следующем массиве [ ");
+            
+            for (int i = 0; i < arrayLenght; i++)
+            {
+                myArray[i] = random.Next(-10000, 10001);
+                Console.Write(myArray[i] + ", ");
+            }
+            Console.WriteLine("\b\b ]\n");
+            
+            result = GetNumberOfPairs(myArray, arrayLenght);
+            
+            Console.WriteLine($"Количество пар: {result}");
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+
+        public static void TASK2()
+        {
+            Console.Title = "Программа 2. Дописать класс для работы с одномерным массивом.";
+            
+            Console.Write("Введите желаемый размер массива: ");
+            int size = int.Parse(Console.ReadLine());
+            Console.Write("Введите первый элемент массива: ");
+            int firstElement = int.Parse(Console.ReadLine());
+            Console.Write("Введите шаг для добавления последующих элементов: ");
+            int step = int.Parse(Console.ReadLine());
+            
+            MyArray a = new MyArray(size, firstElement, step);
+            
+            Console.WriteLine("\nСоздан массив: [ " + a + " ]\n");
+            
+            Console.WriteLine("Сумма элементов массива: " + a.Sum);
+            
+            a.Inverse = -1;
+            
+            Console.WriteLine("Массив с изменёнными знаками: [ " + a + " ]\n");
+            
+            Console.Write("Введите число, на которое будут умножены элементы массива: ");
+            
+            a.Multi = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Массив, умноженный на число: [ " + a + " ]\n");
+            
+            Console.WriteLine("Максимальный элемент массива: " + a.Max);
+            
+            Console.WriteLine("Количество максимальных элементов массива: " + a.MaxCount);
+            Console.ReadKey();
+            
+            Console.WriteLine("=============================================================");
+            
+            Console.WriteLine("\nДалее массив создаётся загрузкой данных из файла.");
+            int[] array = MyArray.LoadFromFile(AppDomain.CurrentDomain.BaseDirectory + "ArayList.txt");
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{array[i]}");
+            }
+            
+            Console.ReadKey();
+            
+            Console.WriteLine("\nМассив сохранен в файл ");
+            MyArray.SaveIntoFile(array, AppDomain.CurrentDomain.BaseDirectory + "ArayListSave.txt");
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+
+        public static void TASK4()
+        {
+             Console.Title = "Программа 4. Реализовать класс для работы с двумерным массивом";
+            
+            Console.Write("Введите желаемое количество строк массива: ");
+            int size1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите желаемое количество столбцов массива: ");
+            int size2 = int.Parse(Console.ReadLine());
+            
+            TwoDimensionalArray twoarray = new TwoDimensionalArray(size1, size2);
+            
+            Console.WriteLine("\nСоздан массив: ");
+            twoarray.PrintTwoArray(twoarray.toString());
+            long sum = 0;
+            twoarray.Sum(out sum);
+            
+            Console.WriteLine("Сумма элементов массива: " + sum);
+            
+            twoarray.SumMoreThan(out sum, twoarray.a[0, 0]);
+            Console.WriteLine("Cумма элементов массива, которые больше, первого элемента: " + sum);
+            
+            Console.WriteLine("Максимальный элемент массива: " + twoarray.Max);
+            
+            Console.WriteLine("Минимальный элемент массива: " + twoarray.Min);
+            
+            string numOfMax = "";
+            twoarray.IndexOfMax(out numOfMax);
+            Console.WriteLine("Номер максимального элемента: " + numOfMax);
+
+            Console.WriteLine("=============================================================");
+            
+            var newTwoArray = new TwoDimensionalArray(4, 4);
+            
+            Console.WriteLine("\nДалее массив создаётся загрузкой данных из файла.");
+            newTwoArray.LoadFromFile(AppDomain.CurrentDomain.BaseDirectory + "LoadTwoArray.txt");
+
+            for (int i = 0; i < newTwoArray.a.GetLength(0); i++)
+            {
+                for (int j = 0; j < newTwoArray.a.GetLength(1); j++)
+                {
+                    Console.Write($"{newTwoArray.a[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+            newTwoArray.SaveIntoFile(AppDomain.CurrentDomain.BaseDirectory + "LoadTwoArraySave.txt");
+            Console.WriteLine("\nМассив сохранен в файл ");
+            
+            Console.ReadKey();
+            Console.Clear();
+            
+        }
+    }
+    
+}
+
